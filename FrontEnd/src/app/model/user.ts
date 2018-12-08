a7771f05bcef99868112eaa75f277b7cc5d41f1b
Alexander Turchanovskiy <turchanovskiya@gmail.com>
2018-12-09 00:39:23 +0100

    Add skeleton for FrontEnd Angular project, Add Materialize framework

---
+++ FrontEnd/src/app/model/user.ts
@@ -0,0 +1,15 @@
+import { Role } from './role';
+import { Course } from './course';
+
+export class User {
+  user_id: number;
+  login: string;
+  password: string;
+  name: string;
+  surname: string;
+  country: string;
+  email: string;
+  city: string;
+  role_id: Role;
+  courseList: Array<Course>;
+}
